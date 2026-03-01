823867f6b222478eea2f005fb7759ffa7f76eeba
Rakan Zeitouni <ng333874@gmail.com>
2026-03-01 16:45:23 +0200

    fix the connnection to pv4 connection

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose",{
+  family: 4
+})
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
