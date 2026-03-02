132d9cc2a4c37d0ff89af3f019ee85bcc01b48be
Rakan Zeitouni <ng333874@gmail.com>
2026-03-02 22:27:31 +0200

    fix the in google id

--- config/passport.js
+++ config/passport.js
@@ -41,4 +41,15 @@ passport.use(new GoogleStrategy({
       return done(err, null);
     }
   }
+
 ));
+passport.serializeUser((user, done) => {
+  done(null, user.id); 
+});passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
